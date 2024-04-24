f41c4be586260c12429820f2ddb1169c56e6e395
Muhangi2 <eliodamutiba@gmail.com>
2024-04-24 15:28:27 +0300

    setupui

---
+++ components/index.js
@@ -0,0 +1,23 @@
+import Card from "./Card";
+import Footer from "./Footer";
+import Headeer from "./Header";
+import HeroSection from "./HeroSection";
+import Selector from "./Selector";
+import SingleCard from "./SingleCard";
+import SwapComponent from "./SwapComponent";
+import SwapField from "./SwapField";
+import Table from "./Table";
+import TokenBalance from "./TokenBalance";
+import TransacationStatus from "./TransacationStatus";
+
+export {
+  Card,
+  Footer,
+  Headeer,
+  HeroSection,
+  Selector,
+  SingleCard,
+  SwapComponent,
+  SwapField,
+  Table,
+};
