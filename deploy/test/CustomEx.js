64e9230213a5a5a353366b7b2d1b5a1f97c69239
Muhangi2 <eliodamutiba@gmail.com>
2024-04-26 23:58:36 +0300

    minttokentests

--- deploy/test/CustomEx.js
+++ deploy/test/CustomEx.js
@@ -1,8 +1,31 @@
-const { wait } = require("@testing-library/user-event/dist/utils");
+// const { wait } = require("@testing-library/user-event/dist/utils");
 const { ethers } = require("hardhat");
 const { expect } = require("chai");
 
+describe("CustomToken", function () {
+  let CustomToken;
+  let customToken;
+  let owner;
+  const TokenName = "ElonMusk";
+  const TokenSymbol = "EM";
 
-describe("DEXExchange", ()=>{
-    
-})
+  beforeEach(async function () {
+    [owner] = await ethers.getSigners();
+
+    CustomToken = await ethers.getContractFactory("CustomToken");
+    customToken = await CustomToken.deploy(TokenName, TokenSymbol);
+    await customToken.deployed();
+  });
+  it("it should deploy with the correct name and symbol", async function () {
+    expect(await customToken.name()).to.equal(TokenName);
+    expect(await customToken.symbol()).to.equal(TokenSymbol);
+
+  });
+
+
+  it("it should mint tokens", async function(){
+    const amount = 1000;
+    await customToken.mint(owner.address, amount);
+    expect(await customToken.balanceOf(owner.address)).to.equal(amount);
+  })
+});
