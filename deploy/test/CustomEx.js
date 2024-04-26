4ed5fca9144f7dceb527e7deb3553d679e482b32
Muhangi2 <eliodamutiba@gmail.com>
2024-04-27 00:53:46 +0300

    transfertokentest

--- deploy/test/CustomEx.js
+++ deploy/test/CustomEx.js
@@ -15,17 +15,22 @@ describe("CustomToken", function () {
     CustomToken = await ethers.getContractFactory("CustomToken");
     customToken = await CustomToken.deploy(TokenName, TokenSymbol);
     await customToken.deployed();
+    // console.log("CustomToken deployed to:", customToken);
   });
   it("it should deploy with the correct name and symbol", async function () {
     expect(await customToken.name()).to.equal(TokenName);
     expect(await customToken.symbol()).to.equal(TokenSymbol);
-
   });
 
+  it("it should  mint initial supply  to the owner", async function () {
+    const ownerBalance = await customToken.balanceOf(owner.address);
+    expect(ownerBalance).to.equal(1000000n * 10n ** 18n);
+  });
 
-  it("it should mint tokens", async function(){
-    const amount = 1000;
-    await customToken.mint(owner.address, amount);
-    expect(await customToken.balanceOf(owner.address)).to.equal(amount);
-  })
+   it("Should transfer tokens between accounts", async function(){
+    const recipient=ethers.Wallet.createRandom().address;
+    await customToken.transfer(recipient, 1000);
+    //lets check 
+   })
+
 });
