4302ffb25b342ee440a3dc43cb8d9c7202e7276a
Muhangi2 <eliodamutiba@gmail.com>
2024-05-09 11:38:27 +0300

    frontend

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -158,7 +158,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3cd9274974ada5e9"; }
+/******/ 		__webpack_require__.h = function() { return "39440cae569baa8e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
