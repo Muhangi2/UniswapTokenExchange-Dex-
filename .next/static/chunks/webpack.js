e354fe0ad35404499428482f7e46ed520fc0d645
Muhangi2 <eliodamutiba@gmail.com>
2024-05-09 10:45:58 +0300

    latest

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -158,7 +158,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4e532047c1f9ccaa"; }
+/******/ 		__webpack_require__.h = function() { return "3cd9274974ada5e9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/TransactionStatus.js
+++ components/TransactionStatus.js
@@ -32,6 +32,27 @@ export default TransactionStatus = () => {
           >
             &#8203;
           </span>
+          <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          enterTo="opacity-100 translate-y-0 sm:scale-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+          leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          >
+            <div className="status inline-block align-bottom py-6 bg-white border
+            rounded-lg text-center overflow-hidden shadow-xl transform transition-all md:w-[35%] sm:my-8 sm:align-middle sm:max-w-lg sm:w-full
+            ">
+              <div className="px-4 py-4 items-center justify-center sm:px-6 sm:flex sm:flex-row-reverse">
+                <Loading>Completing the transaction</Loading>
+              </div>
+           <p className="px-4 py-4 text-black items-center  text-sm justify-center sm:px-6 sm:flex-row-reverse">
+            (Transaction would usually take &lt; 12 secs...)
+           </p>
+            </div>
+
+          </Transition.Child>
         </div>
       </Dialog>
     </Transition.Root>
