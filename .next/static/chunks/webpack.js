a2d1aace24d976b9a8be97cbb54d61685ee87363
Muhangi2 <eliodamutiba@gmail.com>
2024-05-17 23:59:29 +0300

    latest

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -158,7 +158,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1617be3ff14d3c50"; }
+/******/ 		__webpack_require__.h = function() { return "5573a03c4baa604d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Card.js
+++ components/Card.js
@@ -1,6 +1,23 @@
 import React from "react";
+import toast, { Toaster } from "react-hot-toast";
+import {
+  ClipboardIcon,
+  ClipboardCheckIcon,
+  PlusIcon,
+} from "@heroicons/react/outline";
+
+//Internal import
+import { SingleCard, TransactionStatus } from "./index";
+import {
+  getTokenAddress,
+  getTokenBalance,
+  increaseAllowance,
+} from "../utils/context";
+import { useAccount } from "wagmi";
 
 const Card = () => {
+  const { address } = useAccount();
+  console.log(address);
   return <div>Card</div>;
 };
 
